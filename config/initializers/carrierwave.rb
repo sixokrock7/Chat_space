17dfcbda9d4caad14d0e821f3148123af6634f9a
sixokrock7 <sixokrock.7@gmail.com>
2018-08-22 12:49:35 +0900

    fix carriwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,9 +9,9 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'us-east-1'
+    region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'ここにバケット名を入れます'
-  config.asset_host = 'https://s3-ここにリージョン名を入れます(※例、us-east-1).amazonaws.com/ここにバケット名を入れます'
+  config.fog_directory  = 'tatsuki7'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tatsuki7'
 end
